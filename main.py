51d328c37111d18f115162839c13694f73a7c92b
Nikita-Burlakov <nikita225@list.ru>
2021-11-24 16:56:26 +0300

    draw orange circles (ui)

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('My first git-repo')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+from random import choice, randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            for i in range(choice(range(1, 41))):
+                x, y = choice(range(1, 801)), choice(range(1, 601))
+                rad = choice(range(1, 100))
+                qp.drawEllipse(x, y, rad, rad)
+            self.do_paint = False
+            qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
