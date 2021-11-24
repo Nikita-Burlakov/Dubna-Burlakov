34168ec4e500f66aced15ae67e1cecd6846ef324
Nikita-Burlakov <nikita225@list.ru>
2021-11-24 17:15:48 +0300

    draw circles (pyuic)

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 
-from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 
-from random import choice, randint
+from random import choice
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -22,10 +22,12 @@ class MyWidget(QMainWindow):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
-            for i in range(choice(range(1, 41))):
+            for i in range(choice(range(1, 121))):
                 x, y = choice(range(1, 801)), choice(range(1, 601))
                 rad = choice(range(1, 100))
+                red, green, blue = choice(range(256)), choice(range(256)),\
+                                   choice(range(256))
+                qp.setBrush(QColor(red, green, blue))
                 qp.drawEllipse(x, y, rad, rad)
             self.do_paint = False
             qp.end()
