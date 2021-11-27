8dc116c69bb3af8c686ce1663fc997a30cebf94d
Nikita-Burlakov <nikita225@list.ru>
2021-11-27 17:12:51 +0300

    espresso

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import sys
+import sqlite3
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
-
-from random import choice, randint
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
+        uic.loadUi('main.ui', self)
+        self.initUI()
 
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
-            for i in range(choice(range(1, 41))):
-                x, y = choice(range(1, 801)), choice(range(1, 601))
-                rad = choice(range(1, 100))
-                qp.drawEllipse(x, y, rad, rad)
-            self.do_paint = False
-            qp.end()
+    def initUI(self):
+        self.setGeometry(500, 100, 750, 600)
+        self.tableWidget.resize(620, 300)
+        #
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.cur = self.con.cursor()
+        self.data = self.cur.execute('SELECT * FROM coffee').fetchall()
+        #
+        self.tableWidget.setColumnCount(6)
+        self.tableWidget.setRowCount(0)
+        title = ('Сорт', 'Степень обжарки', 'Молотый / в зёрнах',
+                 'Вкус', 'Цена (руб.)', 'Объём упаковки (л)')
+        self.tableWidget.setHorizontalHeaderLabels(title)
+        #
+        for i, row in enumerate(self.data):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row[1:]):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
